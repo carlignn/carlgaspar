53689608e8857c85f06348b28f5ddfd74ae91bd2
carlignn <carlgaspar@protonmail.com>
2021-10-18 16:04:16 +0800

    Tried Other Live Account

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -59,7 +59,7 @@ const TwitchItem = () => {
       <div className="twitch">
         <div className="twitch-video" align="center">
           <iframe
-            src="https://player.twitch.tv/?autoplay=false&channel=aceu&parent=www.carlgaspar.com&parent=carlgaspar.com&parent=carlgaspar.netlilfy.app&parent=www.carlgaspar.netlify.app"
+            src="https://player.twitch.tv/?autoplay=false&channel=lilypichu&parent=www.carlgaspar.com&parent=carlgaspar.com&parent=carlgaspar.netlilfy.app&parent=www.carlgaspar.netlify.app"
             frameborder="0"
             scrolling="no"
             autoplay="true"
